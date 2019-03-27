58a10ced9946c962503e90dd9dc371e6a10a4e47
Ken <Ken@Grytpype.lan>
2019-03-27 16:32:02 +0000

    Remove dead code

--- src/main/java/uk/co/magictractor/oauth/flickr/MyFlickrApp.java
+++ src/main/java/uk/co/magictractor/oauth/flickr/MyFlickrApp.java
@@ -12,8 +12,7 @@ public class MyFlickrApp implements OAuth1Application {
 
 	private final ResourceFileProperties properties = new ResourceFileProperties(MyFlickrApp.class);
 
-	
-// TODO! want connection cache in a superclass, not the file props
+	// TODO! want connection cache in a superclass
 	private OAuth1Connection connection;
 
 	private MyFlickrApp() {
@@ -27,19 +26,6 @@ public class MyFlickrApp implements OAuth1Application {
 	public OAuth1ServiceProvider getServiceProvider() {
 		return Flickr.getInstance();
 	}
-
-//	@Override
-//	public String getScope() {
-//		// return "https://www.googleapis.com/auth/photoslibrary";
-//
-//		// Do not need https://www.googleapis.com/auth/photoslibrary.sharing
-//		// just use sharedAlbums/list to get list of shared albums
-//		// Ah! do want album share info in order to get share Url
-//		return "https://www.googleapis.com/auth/photoslibrary https://www.googleapis.com/auth/photoslibrary.sharing";
-//
-//		// just sharing results in permission denied when listing albums
-//		// return "https://www.googleapis.com/auth/photoslibrary.sharing";
-//	}
 
 	@Override
 	public OAuthConnection getConnection() {
