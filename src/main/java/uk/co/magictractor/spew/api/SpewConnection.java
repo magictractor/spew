089f92ac2c8fd265b9b7e68800357e295eb309ab
Ken D <git@magictractor.co.uk>
2019-08-05 20:01:58 +0100

    Add comment

--- src/main/java/uk/co/magictractor/spew/api/SpewConnection.java
+++ src/main/java/uk/co/magictractor/spew/api/SpewConnection.java
@@ -1,7 +1,19 @@
 package uk.co.magictractor.spew.api;
 
+/**
+ * Application code should not need to interact with SpewConnection instances.
+ */
 public interface SpewConnection {
 
+    /**
+     * <p>
+     * This is used to make authorisation requests as well as API requests.
+     * </p>
+     * <p>
+     * Application code should not use this directly, TODO! doc preferred method
+     * for application code.
+     * </p>
+     */
     SpewResponse request(SpewRequest apiRequest);
 
 }
